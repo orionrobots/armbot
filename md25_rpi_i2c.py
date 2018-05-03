98bd13e3e94ee4bf929fe07a250be9e907088653
Danny Staple <danny@orionrobots.co.uk>
2018-05-03 21:50:37 +0100

    Armbot current code.

---
+++ md25_rpi_i2c.py
@@ -0,0 +1,77 @@
+import smbus
+
+MD25_SPEED1 = 0
+MD25_SPEED2 = 1
+MD25_TURN = 1
+MD25_ENC1A = 2
+MD25_ENC1B = 3
+MD25_ENC1C = 4
+MD25_ENC1D = 5
+MD25_ENC2A = 6
+MD25_ENC2B = 7
+MD25_ENC2C = 8
+MD25_ENC2D = 9
+MD25_BATTV = 10
+MD25_M1_CRNT = 11
+MD25_M2_CRNT = 12
+MD25_SW_REV = 13
+MD25_ACC = 14
+MD25_MODE = 15
+MD25_COMMAND = 16
+
+class Md25pi:
+        def __init__(self, bus, address):
+                self._bus = bus
+                self._address = address
+
+        def write_byte(self, register, byte):
+                """Write a single data byte to a register"""
+                self._bus.write_byte_data(self._address, register, byte)
+
+        def read_register(self, register):
+                """Write a single register byte"""
+                return self._bus.read_byte_data(self._address, register)
+        
+        def get_version(self):
+                """Get device version number"""
+                return self.read_register(MD25_SW_REV)
+
+        def get_battery_volts(self):
+                """Get the battery voltage"""
+                return self.read_register(MD25_BATTV)
+        
+        def stop(self):
+                """Stop all movement"""
+                self.write_byte(MD25_SPEED1, 128)
+                self.write_byte(MD25_SPEED2, 128)
+
+        def set_left(self, speed):
+                """Start left motor. Speed +/- 127"""
+                self.write_byte(MD25_SPEED1, 128 + speed)
+
+        def set_right(self, speed):
+                """Start left motor. Speed +/- 127"""
+                self.write_byte(MD25_SPEED2, 128 + speed)
+
+import time
+
+def main():
+        address = 0x58
+        bus = smbus.SMBus(1)
+        motors = Md25pi(bus, address)
+        try:
+                print "Version is ", motors.get_version()
+                print "Battery voltage is ", motors.get_battery_volts()
+                assert motors.get_battery_volts() >= 80, "Too low - did you turn it on?"
+                motors.set_left(70)
+                motors.set_right(70)
+                time.sleep(1)
+                motors.set_left(50)
+                time.sleep(1)
+                motors.set_left(70)
+                motors.set_right(70)
+                time.sleep(1)
+        finally:
+                motors.stop()
+
+#main()
---
+++ movement_example/movement_example.ino
@@ -0,0 +1,45 @@
+#include <Servo.h>
+
+Servo gripper;
+Servo wrist;
+Servo elbow;
+Servo shoulder;
+Servo base;
+
+#define buttona 7
+#define buttonb 4
+
+void setup() {
+  pinMode(buttona, INPUT);
+  pinMode(buttonb, INPUT);
+  gripper.attach(9);
+  wrist.attach(10);
+  base.attach(11);
+  elbow.attach(12);
+  shoulder.attach(13);
+}
+
+void loop() {
+  if(!digitalRead(buttona)) {
+    gripper.write(60);
+    wrist.write(90);
+    elbow.write(90);
+    shoulder.write(90);
+    base.write(90);
+    
+    delay(1000);
+  //  base.write(;
+    shoulder.write(20);
+    elbow.write(130);
+    delay(300);
+    gripper.write(90);
+    delay(500);
+    
+    base.write(100);
+    shoulder.write(90);
+    elbow.write(90);
+    gripper.write(60);
+    delay(100);
+    gripper.write(90);
+  }
+}
