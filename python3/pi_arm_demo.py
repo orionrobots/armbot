412eacee8803fbf03ba99d0b155d213e58f049ea
Danny Staple <danny@orionrobots.co.uk>
2022-07-20 18:46:32 +0100

    Tidy up - that foldrr isn't needed

--- python3/pi_arm_demo.py
+++
@@ -1,7 +0,0 @@
-"""Quick demonstration of the arm"""
-import uarm_serial
-import serial
-conn = serial.serial_for_url("/dev/ttyUSB0")
-arm = uarm_serial.uArmSerial(conn)
-arm.attach_all()
-uarm_serial.go_demo(arm)
